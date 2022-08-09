077bf1573c19c5932833a32e4cba528b108309ab
Emmanuel Oh <emmaneugene@outlook.com>
2022-08-10 06:49:52 +0800

    Add basic HTML/CSS/JS

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const about = document.querySelector('#about');
+const contact = document.querySelector('#contact');
+const aboutContent = document.querySelector('#about-content');
+const contactContent = document.querySelector('#contact-content');
+
+about.addEventListener('click', () => {
+  const aboutBox = new WinBox({
+    title: About Me
+  })
+})
