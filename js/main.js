6c612d71af58395a0ccb393c68139a61f117308a
Emmanuel Oh <emmaneugene@outlook.com>
2022-08-14 04:28:02 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,140 @@
 const about = document.querySelector('#about');
+const now = document.querySelector('#now');
+const quotes = document.querySelector('#quotes');
+const inspiration = document.querySelector('#inspiration');
+const ideas = document.querySelector('#ideas');
 const contact = document.querySelector('#contact');
+
 const aboutContent = document.querySelector('#about-content');
+const nowContent = document.querySelector('#now-content');
+const quotesContent = document.querySelector('#quotes-content');
+const inspirationContent = document.querySelector('#inspiration-content');
+const ideasContent = document.querySelector('#ideas-content');
 const contactContent = document.querySelector('#contact-content');
 
 about.addEventListener('click', () => {
   const aboutBox = new WinBox({
-    title: About Me
+    title: 'About Me',
+    width: '400px',
+    height: '400px',
+    x: 50,
+    y: 50,
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
   })
 })
+
+now.addEventListener('click', () => {
+  const aboutBox = new WinBox({
+    title: 'Now',
+    width: '400px',
+    height: '400px',
+    x: 150,
+    y: 100,
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: nowContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
+quotes.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Quotes',
+    width: '400px',
+    height: '400px',
+    x: 250,
+    y: 150,
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: quotesContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
+inspiration.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Inspiration',
+    width: '400px',
+    height: '400px',
+    x: 350,
+    y: 200,
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: inspirationContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
+ideas.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Ideas',
+    width: '400px',
+    height: '400px',
+    x: 450,
+    y: 250,
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: ideasContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
+contact.addEventListener('click', () => {
+  const contactBox = new WinBox({
+    title: 'Contact Me',
+    width: '400px',
+    height: '400px',
+    x: 550,
+    y: 300,
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground('#00aa00')
+    },
+    onblur: function () {
+      this.setBackground('#777')
+    },
+  })
+})
+
