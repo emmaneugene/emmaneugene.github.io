f51a853006d966f966e25a0fecd1b6f5a3b45418
Emmanuel Oh <emmaneugene@outlook.com>
2025-01-14 15:39:48 +0800

    feat: Simplify winbox config generation

--- js/main.js
+++ js/main.js
@@ -1,73 +1,37 @@
-config = [
-  {
-    id: "winbox-about",
-    selector: document.querySelector("#about"),
-    title: "About Me",
-    y: "5%",
-    mount: document.querySelector("#about-content"),
-  },
-  {
-    id: "winbox-now",
-    selector: document.querySelector("#now"),
-    title: "Now",
-    y: "10%",
-    mount: document.querySelector("#now-content"),
-  },
-  {
-    id: "winbox-work",
-    selector: document.querySelector("#work"),
-    title: "Working with me",
-    y: "15%",
-    mount: document.querySelector("#work-content"),
-  },
-  {
-    id: "winbox-quotes",
-    selector: document.querySelector("#quotes"),
-    title: "Quotes",
-    y: "20%",
-    mount: document.querySelector("#quotes-content"),
-  },
-  {
-    id: "winbox-cool-stuff",
-    selector: document.querySelector("#cool-stuff"),
-    title: "Cool stuff",
-    y: "25%",
-    mount: document.querySelector("#cool-stuff-content"),
-  },
-  {
-    id: "winbox-topics",
-    selector: document.querySelector("#topics"),
-    title: "Topics",
-    y: "30%",
-    mount: document.querySelector("#topics-content"),
-  },
-  {
-    id: "winbox-contact",
-    selector: document.querySelector("#contact"),
-    title: "Contact",
-    y: "35%",
-    mount: document.querySelector("#contact-content"),
-  },
-];
+const windowCfg = [
+  { name: "about", title: "About me" },
+  { name: "now", title: "What I'm doing now" },
+  { name: "uses", title: "Things I use" },
+  { name: "work", title: "Working with me" },
+  { name: "quotes", title: "Quotes and snippets" },
+  { name: "links", title: "Links to cool stuff" },
+  { name: "themes", title: "Themes that interest me" },
+  { name: "contact", title: "Contact me" },
+]
+let yPos = 5;
 
-config.forEach((e) => {
-  e["selector"].addEventListener("click", () => {
-    if (document.getElementById(e["id"])) {
-      document.getElementById(e["id"]).remove();
+windowCfg.forEach(cfg => {
+  cfg.id = `winbox-${cfg.name}`;
+  cfg.selector = document.querySelector(`#${cfg.name}`);
+  cfg.y = `${yPos}%`;
+  cfg.mount = document.querySelector(`#${cfg.name}-content`);
+  cfg.selector.addEventListener("click", () => {
+    if (document.getElementById(cfg.id)) {
+      document.getElementById(cfg.id).remove();
     }
 
     const box = new WinBox({
-      id: e["id"],
-      title: e["title"],
+      id: cfg.id,
+      title: cfg.title,
       width: "400px",
       height: "400px",
       x: "5%",
-      y: e["y"],
+      y: cfg.y,
       top: 50,
       right: 50,
       bottom: 50,
       left: 50,
-      mount: e["mount"],
+      mount: cfg.mount,
       onfocus: function () {
         this.setBackground("#000");
       },
@@ -76,4 +40,6 @@ config.forEach((e) => {
       },
     });
   });
-});
+
+  yPos += 5;
+})
